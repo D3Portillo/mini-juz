20969b50a472d82333bc58d4ad35743424e7ffd8
D3Portillo <d3portillo@gmail.com>
2025-05-06 03:23:34 +0200

    chores: 2nd try to fix ios bounce

--- app/MainLayout.tsx
+++ app/MainLayout.tsx
@@ -4,9 +4,12 @@ import BottomNavigation from "./BottomNavigation"
 
 export default function MainLayout({ children }: PropsWithChildren) {
   return (
-    <main className="min-h-dvh flex flex-col bg-white w-full max-w-2xl mx-auto">
-      <WithDevAccount>{children}</WithDevAccount>
-      <div className="h-navigation w-full pointer-events-none" />
+    <main className="h-dvh overflow-hidden flex flex-col bg-white w-full max-w-2xl mx-auto">
+      <WithDevAccount>
+        <div className="[&_main]:overflow-auto [&_main]:pb-[4.5rem] [&_main]:max-h-dvh">
+          {children}
+        </div>
+      </WithDevAccount>
       <BottomNavigation />
     </main>
   )
