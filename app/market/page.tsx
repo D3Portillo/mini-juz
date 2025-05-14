c1359dfb6fd59028258aa6de47148c1d2668ae54
D3Portillo <d3portillo@gmail.com>
2025-05-14 15:47:32 +0200

    fix(market/page.tsx): error re-declaring isSuccess

--- app/market/page.tsx
+++ app/market/page.tsx
@@ -60,7 +60,7 @@ export default function PageMarket() {
         })
       }
 
-      const isSuccess = Boolean(
+      isSuccess = Boolean(
         await executeJUZPayment({
           amount: cost, // in JUZ
         })
