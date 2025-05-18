b5eb5caf5b25bd8cde51139cd102e7c87e392d27
D3Portillo <d3portillo@gmail.com>
2025-05-18 23:46:19 +0200

    chores: disable image opt

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,11 @@ import createNextIntlPlugin from "next-intl/plugin"
 
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    unoptimized: true, // Disable image optimization
+  },
+}
 
 const withNextIntl = createNextIntlPlugin("./app/api/i18n/requests.ts")
 export default withNextIntl(nextConfig)
