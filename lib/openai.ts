174c8d1938d552fd6a11b190057216154338970c
D3Portillo <d3portillo@gmail.com>
2025-06-11 17:38:31 +0200

    chores: remove xai

--- lib/openai.ts
+++ lib/openai.ts
@@ -1,5 +1,4 @@
 import { createOpenAI } from "@ai-sdk/openai"
-import { xai } from "@ai-sdk/xai"
 
 export const openai = createOpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -10,17 +9,13 @@ export const GPT4OMini = openai("gpt-4o-mini-2024-07-18")
 export const GPTO3Mini = openai("o3-mini-2025-01-31")
 export const GPT41Mini = openai("gpt-4.1-mini-2025-04-14")
 export const GPT41Nano = openai("gpt-4.1-nano-2025-04-14")
-export const GROK3Mini = xai("grok-3-mini-fast")
 
 const getRandomModel = () => {
-  // Randomly weigh the models, we prefer Mini over Nano,
-  // allow some testing of GROK3
-
+  // Randomly weigh the models, we prefer Mini over Nano
   const sample = Math.random()
 
-  if (sample < 0.8) return GPT41Nano // 80%
-  if (sample < 0.98) return GPT41Mini // 18%
-  return GROK3Mini // 2%
+  if (sample < 0.97) return GPT41Nano // 97%
+  return GPT41Mini
 }
 
 export const getModelForTask = () => {
