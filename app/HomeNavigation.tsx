5279c9aac9e11ad803ebb4df111e36b80f4e2dc2
D3Portillo <d3portillo@gmail.com>
2025-12-23 01:42:17 +0100

    chores(HomeNavigation.tsx): no force show rewards page

--- app/HomeNavigation.tsx
+++ app/HomeNavigation.tsx
@@ -73,7 +73,7 @@ export default function HomeNavigation() {
       )}
 
       <div className="flex-grow" />
-      <JUZCounter asLink href="/rewards" />
+      <JUZCounter />
     </FixedTopContainer>
   )
 }
